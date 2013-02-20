015549fb94b90c5d086b0bea27e6cfba84071fb5
uploada <loadaup@gmail.com>
2013-02-20 20:57:58 +0900

    tiny modification of user.rb and user_spec.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,9 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
 
-  before_save { |user| user.email = email.downcase}
-
+  #before_save { |user| user.email = email.downcase}
+  before_save { email.downcase! }
+  
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
