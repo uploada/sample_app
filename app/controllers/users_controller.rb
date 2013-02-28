db6df35c9d1c93b39cc9898c95d0d48703249f10
uploada <loadaup@gmail.com>
2013-02-28 18:59:46 +0900

    run some personal tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,15 +14,22 @@ class UsersController < ApplicationController
   end  
 
   def new
+    if signed_in?
+      return redirect_to root_path #redirection does not return in rails
+    end
     @user = User.new
   end
 
   def create
+    if signed_in?
+      return redirect_to root_path
+    end
     @user = User.new(params[:user])
     if @user.save
        sign_in @user
        flash[:success] = "welcome to the Sample App!"
        redirect_to @user
+       #redirect_to user_path(@user)
        #redirect_to action: 'show', id: @user.id
     else
       render 'new'
@@ -43,7 +50,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
+    user = User.find(params[:id]).destroy
+    if current_user?(user)
+      return redirect_to root_path
+    end 
     flash[:success] = "User destroyed"
     redirect_to users_path
   end
@@ -66,6 +76,4 @@ class UsersController < ApplicationController
     def admin_user
       redirect_to root_path unless current_user.admin?
     end
- 
-
 end
