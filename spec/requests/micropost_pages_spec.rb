878143216adaee0dd0c0c21ff5bc492c35c21bdc
uploada <loadaup@gmail.com>
2013-03-02 21:33:19 +0900

    solve the exercices at the end of chap1-

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -41,5 +41,55 @@ describe "MicropostPages" do
         expect { click_link "delete" }.to change(Micropost, :count).by(-1)
       end 
     end
+
+    describe "as an incorrect user" do
+      
+      let(:other_user) { FactoryGirl.create(:user) }
+      
+      let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "current user's micropost") }
+      let!(:m2) { FactoryGirl.create(:micropost, user: other_user, content: "other user's micropost") }
+
+      before do
+
+        visit root_path
+
+      end
+     
+      it { should_not have_link('delete', href: "/microposts/#{m2.id}") }
+      it { should have_link('delete', href: "/microposts/#{m1.id}") }
+    end
+
   end
+ 
+  describe "micropost counts and proper pluralization for several microposts" do
+    before { FactoryGirl.create(:micropost, user: user) }
+    before { FactoryGirl.create(:micropost, user: user) }
+    before { visit root_path }
+     
+    it "should count 2 microposts" do
+      should have_content('2 microposts')
+    end  
+  end
+
+  describe "micropost counts and proper pluralization for single micropost" do
+    before { FactoryGirl.create(:micropost, user: user) }
+    before { visit root_path }
+     
+    it "should count 1 micropost" do
+      should have_content('1 micropost')
+    end  
+  end
+
+  describe "micropost pagination" do
+    
+    before(:all) { 30.times { FactoryGirl.create(:micropost, user: user) } }
+    after(:all) { User.delete_all }
+
+    specify { user.microposts.count.should == 30 }
+
+   before { visit root_path } 
+  
+    it { should have_selector('div.pagination') } 
+  end  
+ 
 end
